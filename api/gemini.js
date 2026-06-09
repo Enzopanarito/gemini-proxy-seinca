c9771073a0392fa1c3c4e1c22628f0690a376e0e
Enzopanarito <enzopanarito@gmail.com>
2026-06-08 20:27:27 -0400

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -22,8 +22,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: 'Missing prompt' });
     }
 
-            const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
-
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
     const geminiBody = {
       contents: [{ parts: [{ text: prompt }] }]
     };
